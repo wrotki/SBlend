c235465fe08451f55614ef437fecaba232f8e419
mariusz <wrotki@msn.com>
2015-09-13 18:40:19 -0700

    Some progress with decoding Blender
    Doesn't currently build, snapshot to push a checkpoint

--- src/main/scala/blender/Blend.scala
+++ src/main/scala/blender/Blend.scala
@@ -2,36 +2,72 @@ package blender
 
 import java.nio.charset.Charset
 import scodec.Codec
+import scodec.bits.ByteVector
 import scodec.codecs._
-import scodec.bits._
-import shapeless.HNil
+import shapeless.HList
 
 object BlenderCodecs {
-  case class FileHeader (
-                          //blender: String,
-                          pointerSize: String,
-                          endianness: String,
-                          versionNumber: String
-                          )
-  case class Blend(
-                    fileHeader: FileHeader,
-                    dummy: String,
-                    remainder: String
-                    )
 
   implicit val charSet: Charset = Charset.forName("UTF-8")
 
+  case class FileHeader(
+                         //blender: String,
+                         pointerSize: String,
+                         endianness: String,
+                         versionNumber: String
+                         )
+
   implicit val fileHeader: Codec[FileHeader] = fixedSizeBytes(12, {
-      ("blender" | string.unit("BLENDER")) :~>:
-      ("pointerSize" | fixedSizeBytes(1,string)) ::
-      ("endianness" | fixedSizeBytes(1,string)) ::
-      ("versionNumber" | fixedSizeBytes(3,string))
+    ("blender" | fixedSizeBytes(7, ascii.unit("BLENDER"))) :~>:
+      //      ("blender" | fixedSizeBytes(7,string)) ::
+      ("pointerSize" | fixedSizeBytes(1, string)) ::
+      ("endianness" | fixedSizeBytes(1, string)) ::
+      ("versionNumber" | fixedSizeBytes(3, string))
   }).as[FileHeader]
 
+  case class FileBlockHeader(
+                        code: String,
+                        size: Int,
+                        oldMemoryAddress: Int,
+                        sdnaIndex: Int,
+                        count: Int
+                        )
+
+  implicit val fileBlockHeader: Codec[FileBlockHeader] = {
+    ("code" | fixedSizeBytes(4,string)) >>:~ { implicit code =>
+      ("size" | fixedSizeBytes(4, int32)) ::
+        ("oldMemoryAddress" | fixedSizeBytes(4, int32))
+    }}.as[FileBlockHeader]
+
+  case class FileBlockData(data: ByteVector)
+
+  case class FileBlock(
+                        header: FileBlockHeader,
+                          data: FileBlockData
+                            )
+//  implicit val fileBlock: Codec[FileBlock] = {
+//    ("header" | fileBlockHeader) >>:~ { (hdr: FileBlockHeader) =>
+//      HList("data" | bytes(hdr.size)) }
+//  }.as[FileBlock]
+
+  //  implicit def pcapRecord(implicit ordering: ByteOrdering) = {
+  //    ("record_header"    | pcapRecordHeader                   ) >>:~ { hdr =>
+  //      ("record_data"      | bits(hdr.includedLength * 8) ).hlist
+  //    }}.as[PcapRecord]
+
+  case class Blend(
+                    fileHeader: FileHeader,
+                    records: Vector[FileBlock]
+                    //                    dummy:String,
+                    //                    remainder: ByteVector
+                    )
+
+
   implicit val blend: Codec[Blend] = {
+
     ("fileHeader" | fileHeader) >>:~ { (hdr: FileHeader) =>
-      ("dummy" | fixedSizeBytes(1,string)) ::
-      ("remainder" | string )
+      ("dummy" | fixedSizeBytes(1, string)) ::
+        ("remainder" | bytes)
     }
   }.as[Blend]
 }
--- src/main/scala/blender/Parser.scala
+++ src/main/scala/blender/Parser.scala
@@ -1,15 +1,32 @@
 package blender
 
+import java.io.{File, FileInputStream}
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+
 import blender.BlenderCodecs.Blend
-import scodec.{Codec}
+import scodec.{DecodeResult, Attempt, Codec}
 import scodec.bits._
 
 class Parser {
 
-  def Parse(filePath: String) : Blend = {
-    def bits: BitVector = BitVector.fromMmap(new java.io.FileInputStream(new java.io.File(filePath)).getChannel)
-    val result: Blend = Codec.decode[Blend](bits).require.value
-    result
+  def Parse(filePath: String) : Attempt[DecodeResult[Blend]] = {
+    //println(s"$filePath")
+    val fileChannel: FileChannel = new FileInputStream(new File(filePath)).getChannel
+//    val buf = ByteBuffer.allocateDirect(10)
+//    val count = fileChannel.read(buf)
+//    val cBuf = buf.asCharBuffer
+//
+//    buf.flip
+//    while (buf.hasRemaining) {
+//      println(buf.get.asInstanceOf[Char])
+//    }
+    def bits: BitVector = {
+      BitVector.fromMmap(fileChannel)
+    }
+    //val decoded = Codec.decode[MpegPacket](encoded).require.value
+    val attempt: Attempt[DecodeResult[Blend]] = Codec.decode[Blend](bits)
+    return attempt
   }
 
 }
