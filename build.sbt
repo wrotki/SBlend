2822ce037b2e3a7ce0712847638953a8e639624c
mariusz <wrotki@msn.com>
2016-03-03 07:39:27 +0100

    Transforming SDNA into TypeProperties list
    Preparation to build a type hirerarchy tree

--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  "org.scodec"  %% "scodec-core"  % "1.8.1"
+  "org.scodec"  %% "scodec-core"  % "1.8.1",
+  "org.scalaz" %% "scalaz-core" % "7.2.0"
   )
 
 
--- src/main/scala/SBlend.scala
+++ src/main/scala/SBlend.scala
@@ -2,12 +2,14 @@ import java.io.{FileOutputStream, File, FileInputStream}
 import java.nio.charset.Charset
 
 import blender.BlenderCodecs.{FileBlock, Blend}
-import blender.SDNA.BlenderCodecs.{Structure, StructureDNA}
-import blender.{SDNA, Parser}
+import blender.SDNA.BlenderCodecs.{Field, Structure, StructureDNA}
+import blender._
 import scodec.bits.{ByteVector, BitVector}
 import scodec._
 import shapeless._
 import codecs._
+
+import scalaz.Show
 
 /**
  * Created by mariusz on 4/19/15.
@@ -29,6 +31,7 @@ object SBlend extends App{
 
   case class BlenderHeader(ordering: ByteOrdering)
 
+
   override def main(args: Array[String]): Unit = {
 
 //    val h: ::[Int, ::[String, ::[(String, Int), ::[Int, HNil]]]] = 1 :: "foo" :: ("bar",2) :: 3 :: HNil
@@ -39,6 +42,8 @@ object SBlend extends App{
 
     val parser = new Parser
     val blend = parser.Parse(args(0)).require.value
+
+    println(blend.fileHeader)
 
     //blend.records filter {fb: FileBlock => fb.header.code != "DATA"} foreach { fb: FileBlock => println(fb.header.code)}
 
@@ -78,15 +83,31 @@ object SBlend extends App{
     val sceneTypeName = sdnaDecoded.types(sceneType.name)
     val len = sdnaDecoded.lenghts(sceneType.name)
 
-    println(s"SceneType: $sceneTypeName" + s"length: $len")
+    println(s"SceneType: $sceneTypeName" + s" length: $len")
 
     sceneType.fields foreach {
-      f => println("Field("+sdnaDecoded.names(f.fieldName)+":"+sdnaDecoded.types(f.fieldType)+")")
+      f => println("Field("+sdnaDecoded.names(f.fieldName)+":"+sdnaDecoded.types(f.fieldType)+") length: "+sdnaDecoded.lenghts(f.fieldType ))
     }
 
     println("----------------------")
+    val typeTable = sceneType.fields map {
+      f => TypeProperties(
+        sdnaDecoded.names(f.fieldName),
+        sdnaDecoded.types(f.fieldType),
+        sdnaDecoded.lenghts(f.fieldType),
+        0
+      )
+    }
+    println(typeTable)
+    println("----------------------")
 
-    printStructure(sdnaDecoded,"ID")
+    //printStructure(sdnaDecoded,"ID")
+
+    println(transformStructure(sdnaDecoded,"Scene"))
+    println("----------------------")
+
+    import blender.Show._
+    println(SceneTree.typeTree.drawTree)
 
   }
 
@@ -95,12 +116,38 @@ object SBlend extends App{
     val struct = sdna.structureTypes filter { _.name == typeID} head
 
     val len = sdna.lenghts(typeID)
-    println(s"StructType: $typeName" + s"length: $len")
+    println(s"StructType: $typeName" + s" length: $len")
 
     struct.fields foreach {
-      f => println("Field("+sdna.names(f.fieldName)+":"+sdna.types(f.fieldType)+")")
+      f => println("Field("+sdna.names(f.fieldName)+":"+sdna.types(f.fieldType)+") length: "+sdna.lenghts(f.fieldType))
     }
 
     println("----------------------")
   }
+
+  def transformStructure(sdna: StructureDNA, typeName: String): Seq[FileBlockField] = {
+    if (typeName == "void"){
+      return Seq()
+    }
+    val typeID = sdna.types.indexOf(typeName)
+    val struct = sdna.structureTypes filter { _.name == typeID} headOption
+
+//    val subFields = struct map { s =>
+//      s.fields map {
+//        f => FileBlockField(sdna.names(f.fieldName),sdna.types(f.fieldType),sdna.lenghts(f.fieldType),0,
+//          if ( sdna.names(f.fieldName).startsWith("*")) { Seq() } else { transformStructure(sdna,sdna.types(f.fieldType))} )
+//      }
+//    }
+      //sdna.lenghts(typeID)
+
+
+//    val seqFields = struct.fields map {
+//      f => FileBlockField(sdna.names(f.fieldName),sdna.types(f.fieldType),sdna.lenghts(f.fieldType),0,
+//        if ( sdna.names(f.fieldName).startsWith("*")) { Seq() } else { transformStructure(sdna,sdna.types(f.fieldType))} )
+//    }
+//    seqFields
+    Seq()
+  }
+
+
 }
